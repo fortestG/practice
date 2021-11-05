f02ef7e42d84af8a98ab0062055ee5b4216bebb1
adored <khaykin2001@mail.ru>
2021-11-05 18:29:08 +0300

    аааа

--- Practice/ViewController.swift
+++ Practice/ViewController.swift
@@ -8,14 +8,13 @@
 import UIKit
 
 protocol ViewInputProtocol {
+    
     func updateDisplayData(company: String,
                            symbol: String,
                            price: Double,
                            priceChange: Double,
                            logoURL: URL)
-    
     func resetView()
-    
     func showAlert()
 }
 
@@ -31,33 +30,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var priceChange: UILabel!
     @IBOutlet weak var companyLogo: UIImageView!
     
-    private let companies: [String: String] = ["Apple": "AAPL", "Microsoft": "MCRSFT", "Google": "GOOG", "Amazaon": "AMZN", "Facebook": "FB"]
+    private let companies: [String: String] = ["Apple": "AAPL", "Microsoft": "MCRSFT", "Google": "GOOG", "Amazon": "AMZN", "Facebook": "FB"]
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         networkService = NetworkService(vc: self)
-        
         self.companyPickerView.dataSource = self
         self.companyPickerView.delegate = self
-        
         self.activityIndicator.hidesWhenStopped = true
         requestQuoteUpdate()
     }
     
     private func requestQuoteUpdate() {
+        
         self.activityIndicator.startAnimating()
         self.companyNameLabel.text = "-"
-        
         let selectedRow = self.companyPickerView.selectedRow(inComponent: 0)
         let selectedSymbol = Array(self.companies.values)[selectedRow]
         self.networkService?.sendRequest(company: selectedSymbol)
     }
+    
     private func downloadImage(from url: URL) {
-        networkService?.getData(from: url) { data, response, error in
+        
+        networkService!.getData(from: url) { data, response, error in
             guard let data = data, error == nil else { return }
-            print(response?.suggestedFilename ?? url.lastPathComponent)
             DispatchQueue.main.async() { [weak self] in
                 self?.companyLogo.image = UIImage(data: data)
             }
@@ -66,22 +65,26 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UIPickerViewDataSource {
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
         return self.companies.keys.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
         return Array(self.companies.keys)[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
         self.activityIndicator.startAnimating()
         let selectedSymbol = Array(self.companies.values)[row]
-        self.networkService?.sendRequest(company: selectedSymbol)
+        self.networkService!.sendRequest(company: selectedSymbol)
     }
 }
 
@@ -91,6 +94,7 @@ extension ViewController: UIPickerViewDelegate {
 
 
 extension ViewController: ViewInputProtocol {
+    
     func updateDisplayData(company: String,
                            symbol: String,
                            price: Double,
@@ -113,6 +117,7 @@ extension ViewController: ViewInputProtocol {
     }
     
     func resetView() {
+        
         self.companyNameLabel.text = "_"
         self.symbol.text = "_"
         self.price.text = "_"
@@ -121,6 +126,7 @@ extension ViewController: ViewInputProtocol {
     }
     
     func showAlert() {
+        
         let alertVC = UIAlertController(
             title: "Error",
             message: "Something went wrong",
